9f6141e11594549b7a302b9cfbceec906c9929a7
Don Blair <donblair@gmail.com>
2019-09-07 22:30:46 -0400

    update

--- relay/param_decoder.js
+++ relay/param_decoder.js
@@ -18,7 +18,7 @@ const private_key='8f9a953aa492cc0864a92f2651f530af'
 const num_params=5
 const decoder_divider = 500
 
-var paramList = ['VWV','TTT','PPP','EEEE','CCCC']
+var paramList = ['Volumetric H2O (%)','Soil Temp (C)','Soil Permittivity','Soil Bulk EC (uS/cm)','Soil Pore H2O EC (uS/cm)']
 
 //var paramList = ['a','b','c','d','e']
 

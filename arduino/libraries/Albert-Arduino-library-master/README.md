11e24095b87a635e28d3c74c2cc81576da718463
Don Blair <donblair@gmail.com>
2020-01-21 15:33:07 -0500

    update

---
+++ arduino/libraries/Albert-Arduino-library-master/README.md
@@ -0,0 +1,2 @@
+# Albert-Arduino-library
+This is my Albert library which I use in all my Arduino programs. See the full article here: http://www.avdweb.nl/arduino/albert-library.html.
---
+++ arduino/libraries/Albert-Arduino-library-master/Test_Albert/Test_Albert.ino
@@ -0,0 +1,29 @@
+#include <Streaming.h>
+#include "Albert.h"
+
+const byte ledPin = 3;
+const byte openDrainPin = 2;
+int array[10];
+
+SimpleSoftPWM pwm1;
+
+void setup(void) 
+{ Serial.begin(9600); // Serial instead of SerialUSB for SAMD21
+#if defined(__arm__)
+  while(!Serial); // only for SAMD2, open Serial Monitor first
+#endif 
+  Serial << F("available RAM = ") << availableRAM();
+  Serial << "\ntest comma use , instead of << \" \" << ", 1234, 48, 0, 89; 
+  Serial << "\narrayLenght = " << arrayLenght(array);
+  blinkLed(ledPin, 3, 0); // blinks 3x onLevel=0
+  openDrain(openDrainPin, 0);
+}
+
+void loop(void) 
+{ static int i;
+  //maxLoops(3); // stop after 3 loops  
+  Serial << "\n" << i++;
+  digitalWrite(ledPin, pwm1.pwm(180)); // LED is dimmed 180/255
+}
+
+

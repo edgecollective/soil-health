ce6236922010a5de5aa72f3533d8c47a06fe297a
Don Blair <donblair@gmail.com>
2020-01-21 15:15:00 -0500

    update

---
+++ arduino/chirp-trial-encoder-11a/chirp-trial-encoder-11a.ino
@@ -0,0 +1,356 @@
+/*******************************************************************************
+   The Things Network - Sensor Data Example
+
+   Example of sending a valid LoRaWAN packet with DHT22 temperature and
+   humidity data to The Things Networ using a Feather M0 LoRa.
+
+   Learn Guide: https://learn.adafruit.com/the-things-network-for-feather
+
+   Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+   Copyright (c) 2018 Terry Moore, MCCI
+   Copyright (c) 2018 Brent Rubell, Adafruit Industries
+
+   Permission is hereby granted, free of charge, to anyone
+   obtaining a copy of this document and accompanying files,
+   to do whatever they want with them without any restriction,
+   including, but not limited to, copying, modification and redistribution.
+   NO WARRANTY OF ANY KIND IS PROVIDED.
+ *******************************************************************************/
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+//#include <TinyGPS++.h>
+#include <Adafruit_GPS.h>
+//#define GPSSerial Serial1
+#include "LoraMessage.h" // https://github.com/thesolarnomad/lora-serialization
+
+// Connect to the GPS on the hardware port
+//Adafruit_GPS GPS(&GPSSerial);
+
+//#define GPSECHO false
+
+//uint8_t coords[9];
+//TinyGPSPlus gps;
+//static const uint32_t GPSBaud = 9600;
+
+// include the DHT22 Sensor Library
+//#include "DHT.h"
+
+// DHT digital pin and sensor type
+//#define DHTPIN 10
+//#define DHTTYPE DHT22
+
+int messagecount = 0;
+
+#define DONE 10
+
+//
+// For normal use, we require that you edit the sketch to replace FILLMEIN
+// with values assigned by the TTN console. However, for regression tests,
+// we want to be able to compile these scripts. The regression tests define
+// COMPILE_REGRESSION_TEST, and in that case we define FILLMEIN to a non-
+// working but innocuous value.
+//
+#ifdef COMPILE_REGRESSION_TEST
+#define FILLMEIN 0
+#else
+#warning "You must replace the values marked FILLMEIN with real values from the TTN control panel!"
+#define FILLMEIN (#dont edit this, edit the lines that use FILLMEIN)
+#endif
+
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8] = {0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22};
+void os_getArtEui (u1_t* buf) {
+  memcpy_P(buf, APPEUI, 8);
+}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8] = {0x35, 0x95, 0xb5, 0xbd, 0xde, 0x86, 0x7c, 0x17};
+void os_getDevEui (u1_t* buf) {
+  memcpy_P(buf, DEVEUI, 8);
+}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from the TTN console can be copied as-is.
+static const u1_t PROGMEM APPKEY[16] = { 0x58, 0x0c, 0x7a, 0xc9, 0x85, 0x95, 0x99, 0xf2, 0xca, 0xcb, 0x79, 0x15, 0x0a, 0x45, 0x6f, 0x39 };
+void os_getDevKey (u1_t* buf) {
+  memcpy_P(buf, APPKEY, 16);
+}
+
+
+// payload to send to TTN gateway
+static uint8_t payload[5];
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 10;
+
+// Pin mapping for Adafruit Feather M0 LoRa
+const lmic_pinmap lmic_pins = {
+  .nss = 8,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = 4,
+  .dio = {3, 6, LMIC_UNUSED_PIN},
+  .rxtx_rx_active = 0,
+  .rssi_cal = 8,              // LBT cal for the Adafruit Feather M0 LoRa, in dB
+  .spi_freq = 8000000,
+};
+
+// init. DHT
+//DHT dht(DHTPIN, DHTTYPE);
+
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      {
+        u4_t netid = 0;
+        devaddr_t devaddr = 0;
+        u1_t nwkKey[16];
+        u1_t artKey[16];
+        LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+        Serial.print("netid: ");
+        Serial.println(netid, DEC);
+        Serial.print("devaddr: ");
+        Serial.println(devaddr, HEX);
+        Serial.print("artKey: ");
+        for (size_t i = 0; i < sizeof(artKey); ++i) {
+          if (i != 0)
+            Serial.print("-");
+          Serial.print(artKey[i], HEX);
+        }
+        Serial.println("");
+        Serial.print("nwkKey: ");
+        for (size_t i = 0; i < sizeof(nwkKey); ++i) {
+          if (i != 0)
+            Serial.print("-");
+          Serial.print(nwkKey[i], HEX);
+        }
+        Serial.println("");
+      }
+      // Disable link check validation (automatically enabled
+      // during join, but because slow data rates change max TX
+      // size, we don't use it in this example.
+      LMIC_setLinkCheckMode(0);
+      break;
+    /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_RFU1:
+      ||     Serial.println(F("EV_RFU1"));
+      ||     break;
+    */
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(10);
+
+
+      if (LMIC.txrxFlags & TXRX_ACK) {
+        Serial.println(F("Received ack"));
+
+        
+        for (int k=0;k<2;k++) {
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(10);
+}
+
+
+      }
+      if (LMIC.dataLen) {
+        Serial.println(F("Received "));
+        Serial.println(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_SCAN_FOUND:
+      ||    Serial.println(F("EV_SCAN_FOUND"));
+      ||    break;
+    */
+    case EV_TXSTART:
+      Serial.println(F("EV_TXSTART"));
+      break;
+    default:
+      Serial.print(F("Unknown event: "));
+      Serial.println((unsigned) ev);
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // read the temperature from the DHT22
+   /*
+      int32_t lati = gps.location.lat() * 10000;
+      int32_t lon = gps.location.lng() * 10000;
+      int32_t altitudeGPS = gps.altitude.meters(); // int16_t
+      int8_t hdopGPS = gps.hdop.value(); 
+*/
+  
+
+    LoraMessage message;
+    message
+    .addUnixtime(messagecount);
+
+
+      Serial.println(messagecount);
+      //Serial.println(lon);
+      //Serial.println(altitudeGPS);
+      //Serial.println(hdopGPS);
+
+       LMIC_setTxData2(1,message.getBytes(), message.getLength(), 1);
+
+//delay(3000);
+
+/*
+for (int k=0;k<2;k++) {
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+}
+*/
+
+  messagecount++;
+  
+//delete message;
+
+    // prepare upstream data transmission at the next possible time.
+    // transmit on port 1 (the first parameter); you can use any value from 1 to 223 (others are reserved).
+    // don't request an ack (the last parameter, if not zero, requests an ack from the network).
+    // Remember, acks consume a lot of network resources; don't ask for an ack unless you really need it.
+   // LMIC_setTxData2(1, payload, sizeof(payload), 0);
+
+   
+  //  digitalWrite(DONE, LOW);
+
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+
+ pinMode(LED_BUILTIN, OUTPUT);
+ 
+  //pinMode(DONE, OUTPUT);
+   //digitalWrite(DONE, HIGH);
+ delay(5000);
+  //while (! Serial);
+  Serial.begin(9600);
+  Serial.println(F("Starting"));
+
+
+
+  delay(1000);
+
+  // Ask for firmware version
+
+  
+  //dht.begin();
+
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+  // Disable link-check mode and ADR, because ADR tends to complicate testing.
+  LMIC_setLinkCheckMode(0);
+  // Set the data rate to Spreading Factor 7.  This is the fastest supported rate for 125 kHz channels, and it
+  // minimizes air time and battery power. Set the transmission power to 14 dBi (25 mW).
+  LMIC_setDrTxpow(DR_SF7, 14);  // is DR_SF12 "long range"???
+  
+  // in the US, with TTN, it saves join time if we start on subband 1 (channels 8-15). This will
+  // get overridden after the join by parameters from the network. If working with other
+  // networks or in other regions, this will need to be changed.
+  LMIC_selectSubBand(1);
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+}
+
+void loop() {
+  // we call the LMIC's runloop processor. This will cause things to happen based on events and time. One
+  // of the things that will happen is callbacks for transmission complete or received messages. We also
+  // use this loop to queue periodic data transmissions.  You can put other things here in the `loop()` routine,
+  // but beware that LoRaWAN timing is pretty tight, so if you do more than a few milliseconds of work, you
+  // will want to call `os_runloop_once()` every so often, to keep the radio running.
+  os_runloop_once();
+
+  // if millis() or timer 
+  
+}
+
+// degree-minute format to decimal-degrees
+double convertDegMinToDecDeg (float degMin) {
+  double min = 0.0;
+  double decDeg = 0.0;
+ 
+  //get the minutes, fmod() requires double
+  min = fmod((double)degMin, 100.0);
+ 
+  //rebuild coordinates in decimal degrees
+  degMin = (int) ( degMin / 100 );
+  decDeg = degMin + ( min / 60 );
+ 
+  return decDeg;
+}
